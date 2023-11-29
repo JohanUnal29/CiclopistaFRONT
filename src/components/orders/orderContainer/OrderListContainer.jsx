440140174a59a5810173c2246224a01e4928aa8d
JohanUnal29 <111397882+JohanUnal29@users.noreply.github.com>
2023-11-28 21:44:54 -0500

    rol

--- src/components/orders/orderContainer/OrderListContainer.jsx
+++ src/components/orders/orderContainer/OrderListContainer.jsx
@@ -33,7 +33,6 @@ export default function OrderListContainer() {
         rol: rol,
       };
       setUser(userData);
-      console.log("gonorrea usuario en orders", userData.uid);
     });
   }
 
