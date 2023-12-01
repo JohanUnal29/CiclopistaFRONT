2378dfd97fbcb5ce718ccb08d99fbfb12f03ab9f
JohanUnal29 <111397882+JohanUnal29@users.noreply.github.com>
2023-12-01 10:34:14 -0500

    implementando AutCntext

--- src/components/orders/orderContainer/OrderListContainer.jsx
+++ src/components/orders/orderContainer/OrderListContainer.jsx
@@ -3,11 +3,9 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import OrderList from "./OrderList";
 import NavBar3 from "../../subcomponents/navbar/Navbar3";
-import firebaseApp from "../../../firebase/Credentials";
-import { getAuth, onAuthStateChanged} from "firebase/auth";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
-const auth = getAuth(firebaseApp);
-const firestore = getFirestore(firebaseApp);
+
+//firebase
+import { useAuth } from "../../../context/AuthContext";
 
 export default function OrderListContainer() {
 
@@ -16,38 +14,7 @@ export default function OrderListContainer() {
 
   const apiURL = process.env.REACT_APP_API_URL;
 
-  const [user, setUser] = useState(null);
-
-  async function getRol(uid) {
-    const docuRef = doc(firestore, `usuarios/${uid}`);
-    const docuCifrada = await getDoc(docuRef);
-    const infoFinal = docuCifrada.data().rol;
-    return infoFinal;
-  }
-
-  function setUserWithFirebaseAndRol(usuarioFirebase) {
-    getRol(usuarioFirebase.uid).then((rol) => {
-      const userData = {
-        uid: usuarioFirebase.uid,
-        email: usuarioFirebase.email,
-        rol: rol,
-      };
-      setUser(userData);
-    });
-  }
-
-  onAuthStateChanged(auth, (usuarioFirebase) => {
-    if (usuarioFirebase) {
-      //funcion final
-
-      if (!user) {
-        setUserWithFirebaseAndRol(usuarioFirebase);
-      }
-    } else {
-      setUser(null);
-    }
-  });
-
+  const { user, loading } = useAuth(); 
 
   useEffect(() => {
     // Verifica si 'user' no es null antes de realizar la solicitud a la API
