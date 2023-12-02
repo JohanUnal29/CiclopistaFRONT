902f6f51d6d83ac703e2dbec7956dbffa84ac380
JohanUnal29 <111397882+JohanUnal29@users.noreply.github.com>
2023-12-01 19:47:12 -0500

    tickets AutCntext

--- src/components/orders/orderDetailContainer/OrderDetailContainer.jsx
+++ src/components/orders/orderDetailContainer/OrderDetailContainer.jsx
@@ -3,37 +3,35 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import NavBar3 from "../../subcomponents/navbar/Navbar3";
 import OrderDetail from "./OrderDetail.jsx";
-
-//firebase
 import { useAuth } from "../../../context/AuthContext.jsx";
+
 export default function OrderDetailContainer() {
-    const [order, setOrder] = useState(null);
-    const id = useParams().id;
-    console.log(id);
-    
+  const [order, setOrder] = useState(null);
+  const id = useParams().id;
+  console.log(id);
 
-    const apiURL = process.env.REACT_APP_API_URL;
-    //firebase
-    const { user, loading } = useAuth(); 
+  const apiURL = process.env.REACT_APP_API_URL;
 
-    console.log("usuario: "+user.uid);
+  const { user, loading } = useAuth();
 
-    useEffect(() => {
-        if (user) {
-            axios.get(`${apiURL}/api/purchase/id/${id}/${user.uid}`).then(res => {
-                setOrder(res.data.payload);
-                console.log("Respuesta del servidor:", res.data.payload);
-            }).catch(err => {
-                console.log(err);
-            });
-        }
-    }, [id, user]);
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(`${apiURL}/api/purchase/id/${id}/${user.uid}`)
+        .then((res) => {
+          setOrder(res.data.payload);
+          console.log("Respuesta del servidor:", res.data.payload);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [id, user]);
 
-    
-    return (
-        <div>
-            <NavBar3></NavBar3>
-            {order !== null && <OrderDetail order={order} />}
-        </div>
-    )
+  return (
+    <div>
+      <NavBar3></NavBar3>
+      {order !== null && <OrderDetail order={order} />}
+    </div>
+  );
 }
