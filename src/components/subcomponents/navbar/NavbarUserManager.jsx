531b5f0d1f9e458d9500c26a1df06b94ac603321
JohanUnal29 <johan.ardilah@gmail.com>
2023-12-14 19:26:15 -0500

    navbars

--- src/components/subcomponents/navbar/NavbarUserManager.jsx
+++ src/components/subcomponents/navbar/NavbarUserManager.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import {NavDropdown, Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import Swal from "sweetalert2";
