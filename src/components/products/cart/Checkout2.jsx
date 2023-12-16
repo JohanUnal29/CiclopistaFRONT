17f4730e9c2e2c1a7ac286168c65279680bb225d
JohanUnal29 <johan.ardilah@gmail.com>
2023-12-16 12:44:53 -0500

    Wompi Api

--- src/components/products/cart/Checkout2.jsx
+++ src/components/products/cart/Checkout2.jsx
@@ -4,6 +4,7 @@ import { CartContext } from "../../../context/CartContext";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
+import Wompi from "./Wompi.jsx";
 
 export default function Checkout2() {
   const { carrito, precioTotal, vaciarCarrito } = useContext(CartContext);
@@ -80,26 +81,7 @@ export default function Checkout2() {
     vaciarCarrito();
   };
 
-  const WompiButton = () => {
-    useEffect(() => {
-      // Cargar el script de Wompi
-      const script = document.createElement('script');
-      script.src = 'https://checkout.wompi.co/widget.js';
-      script.setAttribute('data-render', 'button');
-      script.setAttribute('data-public-key', 'pub_test_X0zDA9xoKdePzhd8a0x9HAez7HgGO2fH');
-      script.setAttribute('data-currency', 'COP');
-      script.setAttribute('data-amount-in-cents', '4950000');
-      script.setAttribute('data-reference', '4XMPGKWWPKWQ');
-      script.setAttribute('data-signature:integrity', '37c8407747e595535433ef8f6a811d853cd943046624a0ec04662b17bbf33bf5');
   
-      // Añadir el script al head del documento
-      document.head.appendChild(script);
-  
-      // Limpieza al desmontar el componente
-      return () => {
-        document.head.removeChild(script);
-      };
-    }, []);}
 
   return (
     <>
@@ -116,7 +98,7 @@ export default function Checkout2() {
             <h5>Orden</h5>
             {ordenCompleta ? (
               // Renderizar el formulario de pago cuando la orden está completa
-              <WompiButton/>
+              <Wompi/>
             ): (
 
                 <Form>
