5f0b2f7d797d2dff0d6fa93c14a0cccbff96fdf5
JohanUnal29 <johan.ardilah@gmail.com>
2024-01-12 16:58:09 -0500

    Comprobante de pago

--- src/components/products/cart/wompiComponents/PDF.jsx
+++ src/components/products/cart/wompiComponents/PDF.jsx
@@ -37,60 +37,12 @@ export default function PDF() {
         <Document>
             <Page size="A4" style={styles.page}>
                 <View style={styles.section}>
-                    <Text>Section #1</Text>
+                    <Text>{status}</Text>
                 </View>
                 <View style={styles.section}>
                     <Text>Section #2</Text>
                 </View>
             </Page>
-            <Container>
-
-                {status === "APPROVED" && (
-                    <div><FcApproval /> Transacción Aprobada</div>
-                )}
-                {(status === "DECLINED") && (
-                    <div><FcHighPriority /> Transacción Rechazada, intenta de nuevo con el mismo u otro medio de pago</div>
-                )}
-                {(status === "ERROR") && (
-                    <div><FcHighPriority /> {status_message}</div>
-                )}
-                {status === "PENDING" && (
-                    <div><FcHighPriority /> Transacción Pendiente <br />
-                        Si deseas puedes esperar en esta pestaña para recibir una respuesta final e imprimir tu comprobante<br />
-                        o en un rato se notificara a tu correo correo el estado final por parte de Wompi</div>
-                )}
-                <Table striped style={{ textAlign: 'center' }}>
-                    <tr style={{ backgroundColor: '#FF4545' }}>
-                        <th colSpan={1}>Pedido a Nombre de {nameOrder}</th>
-                    </tr>
-                    <tr style={{ backgroundColor: '#A5FF45' }}>
-                        <th colSpan={1}>Información de la transacción</th>
-                    </tr>
-                    <tbody style={{ textAlign: 'center' }}>
-                        <tr>
-                            <td><b>Transacción #</b>{tab}{tab}{tab}{transaccionId}</td>
-                        </tr>
-                        <tr>
-                            <td><b>Referencia</b>{tab}{tab}{tab}{referencia}</td>
-                        </tr>
-                        <tr>
-                            <td><b>Email</b>{tab}{tab}{tab}{emailPay}</td>
-                        </tr>
-                    </tbody>
-                    <tr style={{ backgroundColor: '#A5FF45' }}>
-                        <th colSpan={1}>Información del pagador</th>
-                    </tr>
-                    <tbody>
-                        <tr>
-                            <td><b>Nombre</b>{tab}{tab}{tab}{namePay}</td>
-                        </tr>
-                        <tr>
-                            <td><b>Teléfono</b>{tab}{tab}{tab}{numero}</td>
-                        </tr>
-                    </tbody>
-                </Table>
-
-            </Container>
         </Document>
     )
 }
