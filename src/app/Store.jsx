6f30adf3559ce40bdd97fd22f02cc991615b33f9
JohanUnal29 <johan.ardilah@gmail.com>
2024-05-15 22:45:27 -0500

    Modal product

--- src/app/Store.jsx
+++ src/app/Store.jsx
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import wompiReducer from "../../src/features/wompi/WompiSlice.jsx"
 import productReducer from "../../src/features/product/ProductSlice.jsx"
+import modalReducer from "../../src/features/modal/ModalSlice.jsx"
 
 export const store = configureStore({
     reducer: {
         wompi: wompiReducer,
-        product: productReducer
+        product: productReducer,
+        modal: modalReducer
     },
 })
