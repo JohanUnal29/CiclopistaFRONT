140585b40f6f923045b9e1089aafc91cdc2818dd
JohanUnal29 <johan.ardilah@gmail.com>
2024-05-24 21:46:10 -0500

    Modal update product

--- src/components/products/productContainer/UpdateProducts.jsx
+++ src/components/products/productContainer/UpdateProducts.jsx
@@ -31,7 +31,6 @@ export default function UpdateProducts({ product }) {
     });
 
     useEffect(() => {
-        if (product) {
             setForm({
                 title: product.title,
                 description: product.description,
@@ -43,7 +42,6 @@ export default function UpdateProducts({ product }) {
                 subCategory: product.subCategory,
                 image: product.image
             });
-        }
     }, [product]);
 
     const handleChange = (e) => {
